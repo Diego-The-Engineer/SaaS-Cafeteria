2e1da0889a5e766d67d1e270d65eda51626c7641
Diego-The-Engineer <diego.gomez01@anahuac.mx>
2026-06-15 12:59:39 +0800

    Commit inicial: Proyecto SaaS Cafetería

---
+++ main.py
@@ -0,0 +1,102 @@
+import os
+from datetime import datetime
+from bson import ObjectId
+from fastapi import HTTPException
+from typing import List, Optional
+from  models import Model_producto, Response_producto, Item_pedido, Create_pedido, Response_pedido, Response_msg
+from database import db
+from fastapi import FastAPI
+
+app = FastAPI(title="Cafeteria")
+
+@app.get("/")
+def root_func():
+        return {"Message": "Bienvenido"}
+@app.get("/test-db")
+async def test_db():
+        try:
+                await db.command("ping")
+                return {"status": "ok", "msg": "connection succesfully"}
+        except Exception as e:
+                return {"status": "error", "msg": "Error connection", "details": str(e)}
+
+
+@app.post("/productos", response_model=Response_producto)
+async def post_productos(producto: Model_producto):
+        producto_dict = producto.model_dump()
+        resultado = await db["productos"].insert_one(producto_dict)
+        producto_dict["id"] = str(resultado.inserted_id)
+        return producto_dict
+
+
+@app.get("/sus_productos", response_model=List[Response_producto])
+async def get_productos(disponible: Optional[bool] = None):
+        filtro = {}
+        if disponible is not None:
+                filtro["disponible"] = disponible
+        cursor = db["productos"].find(filtro)
+        productos_db = await cursor.to_list(length=100)
+        for prod in productos_db:
+                prod["id"] = str(prod["_id"])
+        return productos_db
+
+@app.post("/pedidos", response_model=Response_pedido)
+async def post_pedidos(pedidos: Create_pedido):
+	total = 0.0
+	items_detallados = []
+	
+	for item in pedidos.items:
+		producto_db = await db["productos"].find_one({"_id": ObjectId(item.producto_id)})
+		if not producto_db:
+			raise HTTPException(status_code=404, detail=f"Error, producto no encontrado")
+		subtotal = float(producto_db["precio_unitario"]) * item.cantidad
+		total += subtotal
+		items_detallados.append({
+			"producto_id": str (producto_db["_id"]),
+			"nombre": str (producto_db["nombre"]),
+			"cantidad": item.cantidad,
+			"precio_unitario": float (producto_db["precio_unitario"]),
+			"subtotal": subtotal
+		})
+	ticket = {
+		"fecha": datetime.utcnow(),
+		"items": items_detallados,
+		"total_pagado": total
+	}
+	
+	resultado = await db["pedidos"].insert_one(ticket)
+	ticket["id"] = str(resultado.inserted_id)
+
+	return ticket
+
+@app.delete("/pedidos/{id}", response_model=Response_msg)
+async def delete_pedido(id: str):
+	collection = db["productos"]
+	oid = ObjectId(id.strip('"'))
+	pedidos = await db["productos"].find_one({"_id": oid})
+	if pedidos is not None:
+		filtro = {"_id": oid}
+		resultado = await collection.delete_one(filtro)
+		return {"msg": "Elemento eliminado correctamente"}
+	else:
+		raise HTTPException(status_code=404, detail="Producto no encontrado") 
+
+@app.patch("/pedidos/{id}/items/{producto_id}")
+async def remove_item_from_pedido(id: str, producto_id: str):
+    oid = ObjectId(id.strip('"'))
+    resultado = await db["pedidos"].update_one(
+        {"_id": oid},
+        {"$pull": {"items": {"producto_id": producto_id}}}
+    )
+    if resultado.modified_count == 0:
+        raise HTTPException(status_code=404, detail="Pedido no encontrado o el producto no estaba en el pedido")
+    pedido_actualizado = await db["pedidos"].find_one({"_id": oid})
+    nuevo_total = sum(item["subtotal"] for item in pedido_actualizado["items"])
+    await db["pedidos"].update_one(
+        {"_id": oid},
+        {"$set": {"total_pagado": nuevo_total}}
+    )
+    return {
+        "msg": "Producto eliminado y total actualizado correctamente", 
+        "nuevo_total": nuevo_total
+    }
