2e7ed706a560b017dc11aabd9d63b6abdf2a79d9
Diego-The-Engineer <diego.gomez01@anahuac.mx>
2026-06-18 07:42:47 +0800

    Actualizaciones...

--- auth.py
+++ auth.py
@@ -1,16 +1,19 @@
 from datetime import datetime, timedelta, timezone
 from typing import Annotated
-
+import os
 import jwt
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jwt.exceptions import InvalidTokenError
 from pwdlib import PasswordHash
 from pydantic import BaseModel
-
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = os.getenv( "e37c5f524093c5ad78608f19ae80b981ff5f06d8a5cf25a974efed6013c170c7")
+if not SECRET_KEY:
+	raise RunTimeError(
+		"SECRET_KEY no encontrada"
+	)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
