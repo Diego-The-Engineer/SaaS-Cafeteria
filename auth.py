4b4db5de8433c980c6378d730f45a0736eb1fe4b
Diego-The-Engineer <diego.gomez01@anahuac.mx>
2026-06-18 07:49:16 +0800

    Cambiando servicios

--- auth.py
+++ auth.py
@@ -20,9 +20,9 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 fake_users_db = {
     "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
+        "username": "admin",
+        "full_name": "admin",
+        "email": "admin@example.com",
         "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
         "disabled": False,
     }
